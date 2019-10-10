92c4bac2cfc007f0eb083a9bff4d84333315343f
Gayathri Buddhika <39160904+gayathribuddhika@users.noreply.github.com>
2019-10-10 19:12:21 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,36 @@
+x = 25
+y = x % 2
+if y == 1:
+    print("The number is Odd")
+else:
+    print("The number is Even")
+
+age = 25
+if age < 20:
+    print("Successful")
+else:
+    print("Not Successful")
+
+name = "Harry"
+
+if name is "ron":
+    print("Hello ron")
+elif name is "Lucy":
+    print("Hello Lucy")
+else:
+    print("Hello "+name)
+
+marks = 65
+
+if marks >= 75:
+    print("Grade = A")
+elif marks >= 65:
+    print("Grade = B")
+elif marks >= 55:
+    print("Grade = C")
+elif marks >= 35:
+    print("Grade = D")
+else:
+    print("Grade = E")
+
+
